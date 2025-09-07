e1e6f3c591bc8cc5d5becfa70eb906830ca6492a
camgoo2 <114447057+camgoo2@users.noreply.github.com>
2025-09-07 11:04:06 +1000

    Initial Commit

---
+++ bounce/bounceApp.swift
@@ -0,0 +1,17 @@
+//
+//  bounceApp.swift
+//  bounce
+//
+//  Created by Cameron Goodhue on 07/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct bounceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bounceTests/bounceTests.swift
@@ -0,0 +1,17 @@
+//
+//  bounceTests.swift
+//  bounceTests
+//
+//  Created by Cameron Goodhue on 07/09/2025.
+//
+
+import Testing
+@testable import bounce
+
+struct bounceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
